867a69ab23d33d8a74c9e1b19292ebf41fd23bf1
user <webworking66@mail.ru>
2021-02-07 11:27:07 +0400

    Created Blocks

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const router = require('./router/router')
 
 app.set('view engine', 'ejs');
 
-app.use(Express.urlencoded({extended: true}))
+app.use(Express.urlencoded({extended: true}));
 app.use(Express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
 
 
-const PORT = 3000
+const PORT = 5000
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}...`)
